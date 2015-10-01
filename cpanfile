f0eca12c0f8abe5220cbd49e63d0a47c88dc3532
Patrick Hochstenbach <patrick.hochstenbach@ugent.be>
2015-10-01 13:31:56 +0200

    Adding support for other metadataPrefixes and urls

--- cpanfile
+++ cpanfile
@@ -1,5 +1,7 @@
-requires Catmandu
-requires Catmandu::DBI
-requires Catmandu::OAI
-requires Net::Twitter::Lite::WithAPIv1_1
+requires 'Catmandu' , 0;
+requires 'Catmandu::DBI' , 0;
+requires 'Catmandu::OAI' , 0;
+requires 'Catmandu::MARC', 0;
+requires 'Net::Twitter::Lite::WithAPIv1_1' , 0;
+requires 'DBD::SQLite', 0;
 
--- tweet.pl
+++ tweet.pl
@@ -4,6 +4,7 @@ use Catmandu::Sane;
 use Catmandu qw(:load);
 use POSIX qw(strftime);
 use Net::Twitter::Lite::WithAPIv1_1;
+use Data::Dumper;
 use Getopt::Long;
 
 sub _timestamp {
@@ -12,26 +13,34 @@ sub _timestamp {
 
 my $oai;
 my $csv;
+my $fix_file;
 my $file;
+my $url;
 my $set;
+my $metadataPrefix = 'oai_dc';
 my $from;
 my $until;
 my $limit = Catmandu->config->{tweet_limit};
 my $v;
 my $x;
+my $debug;
 my $help;
 
 GetOptions(
+    'fix=s' => \$fix_file,
     'oai' => \$oai,
     'csv' => \$csv,
     'file=s' => \$file,
+    'url=s' => \$url,
     'set=s' => \$set,
+    'metadataPrefix=s' => \$metadataPrefix,
     'from=s' => \$from,
     'until=s' => \$until,
     'limit=i' => \$limit,
     'verbose|v' => \$v,
     'execute|x' => \$x,
     'help|h|?' => \$help,
+    'debug' => \$debug,
 );
 
 if ($help || (!$oai && !$csv)) {
@@ -44,6 +53,9 @@ Options:  oai:     tweet records harvested from a OAI endpoint (defined in
           csv:     tweet records from a csv file (format '_id,title',
                    '_id,url,title' or '_id,tweet' defined in catmandu.yml)
           file:    path to csv file (default is stdin)
+          fix:     fix file to operate on the record (optional)
+          url:     oai baseURL to use (optional)
+          metadataPrefix: oai metadataPrefix to use (optional)
           set:     oai set to use (optional)
           from:    lower bound for oai harvest (optional)
           until:   upper bound for oai harvest (optional)
@@ -56,6 +68,7 @@ Options:  oai:     tweet records harvested from a OAI endpoint (defined in
           ?:       see help
           execute: actually send the tweets
           x:       see execute
+          debug:   debugging messages
 See the catmandu.yml file for more configuration options.
 EOF
     exit 0;
@@ -75,12 +88,33 @@ my %in_opts;
 if ($csv) {
     $in_opts{file} = $file if $file;
 } else {
+    $in_opts{url} = $url if $url;
+    $in_opts{metadataPrefix} = $metadataPrefix if $metadataPrefix;
     $in_opts{set} = $set if $set;
     $in_opts{from} = $from if $from;
     $in_opts{until} = $until if $until;
 }
 
+print STDERR "importer $in_name: " . Dumper(\%in_opts) if $debug;
+
 my $in = Catmandu->importer($in_name, %in_opts);
+
+my $fixer;
+
+if (defined $fix_file) {
+  $fixer = Catmandu->fixer($fix_file);
+}
+elsif ($csv) {
+  $fixer = Catmandu->fixer('csv');
+}
+elsif ($oai) {
+  $fixer = Catmandu->fixer($metadataPrefix);
+}
+else {
+  $fixer = Catmandu->fixer('null()');
+}
+
+print STDERR "fixer: $fixer\n" if $debug;
 
 my $log = Catmandu->store('tweet_log')->bag('tweets');
 
@@ -88,18 +122,23 @@ my $log_file = Catmandu->exporter('tweet_log');
 
 my $url_tmpl = Catmandu->config->{url_template};
 
-$in->reject(sub { $log->get($_[0]->{_id}) })
+$fixer->fix($in)->reject(sub { $log->get($_[0]->{_id}) })
     ->take($limit)
     ->each(sub {
         my $rec = $_[0];
 
+        print STDERR "Got: " . Dumper($rec) if $debug;
+
+        die "need an _id" unless $rec->{_id};
+
         my $id = $rec->{_id};
 
         my $tweet = $rec->{tweet} || do {
-            my $url = $rec->{url} || sprintf($url_tmpl, $id);
+            die "need a url and title" unless ($rec->{url} && $rec->{title});
+
+            my $url = $rec->{url};
 
             my $title = $rec->{title};
-            $title = $title->[0] if ref $title;
 
             my $msg;
             if (length($url) + length($title) + 1 > 140) {
@@ -117,6 +156,7 @@ $in->reject(sub { $log->get($_[0]->{_id}) })
         };
 
         if ($x) {
+            print STDERR "Sending tweet: $tweet\n" if $debug;
             try {
                 $twitter_client->update($tweet);
                 $log->add($log_data);
@@ -124,6 +164,7 @@ $in->reject(sub { $log->get($_[0]->{_id}) })
                 warn "[ERROR] $_";
             }
         }
+
         if ($v) {
            $log_file->add($log_data); 
         }
